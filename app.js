59f8872b207be79f30aa16917ce734d0135d690c
Faizieva Venera <v.faizyeva@gmail.com>
2023-04-25 00:20:44 +0300

    feat: add eslint

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-let keyboard = []
+const keyboardEng = 
+['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
+ 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Delete',
+ 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'Enter',
+ 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/','ArrowUp', 'Shift',
+ 'Control', 'Win', 'Alt', ' ', 'Alt','ArrowLeft','ArrowDown', 'ArrowRight', 'Control']
+ 
 document.onkeydown = function(e){
-    
-    console.log(window.event, window.event.key, String.charAt(window.event.keyCode))
+  keyboard.push(window.event.key)
+  console.log(keyboard)
 }
